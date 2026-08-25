8a6736a894ce2ac15a99a4a988f6f29fa645e414
Piyush Kumar <Piyushzane4@gmail.com>
2026-08-25 15:05:40 +0300

    grade

--- introduction.py
+++ introduction.py
@@ -5,3 +5,15 @@ if name == ("piyush"):
      print("same name!!")
 else:
      print("sorry different name")
+
+grade = int(input("what marks have you gotten"))
+if grade >= 90:
+     print("you have gotten an A")
+elif grade >= 80:
+     print("you have gotten an B")
+elif grade >= 70:
+     print("you have gotten an C")
+elif grade >= 60:
+     print("you have gotten a  D")
+else:
+     print("fail")
