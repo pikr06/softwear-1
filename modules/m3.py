599b9df1395bee5c13a83d07e9255b65208278c3
Piyush Kumar <Piyushzane4@gmail.com>
2026-08-28 14:22:11 +0300

    space

---
+++ modules/m3.py
@@ -0,0 +1,32 @@
+#1
+#print("enter your name")
+#name = input()
+#print("hello",name)
+
+#2
+#print("what is the reduis of the circle?")
+#radi = int(input())
+#area = int(input(3.14*radi*radi))
+#print = (area)
+
+#3
+#print("what is the length of the rectangle ")
+#length = int(input())
+#width = int(input("what is the width of the rectangle"))
+#peremeter = int((2*length)+(2*width))
+#print("the peremeter of the rectangle is",peremeter)
+#area = int(length*width)
+#print("the area of the rectangle is",area)
+
+#4
+num1=int(input("what is your first num"))
+num2=int(input("what is your second num"))
+num3=int(input("what is your third num"))
+sum=int(num1+num2+num3)
+product=int(num1*num2*num3)
+avg=((num1+num2+num3)/3)
+print("your answers are", sum, product, avg)
+
+#5
+
+         
---
+++ modules/m4.py
@@ -0,0 +1,8 @@
+print("what is the length of the zander")
+zander = int(input())
+if zander >= 42:
+    print("if has fulfilled the size limit well done")
+else:
+    print("try again")
+
+    
