a80d4c9ed3e982ee5defc3bbbe07518d5b6e5ef0
Piyush Kumar <Piyushzane4@gmail.com>
2026-09-06 21:40:12 +0300

    chnage

---
+++ extra work/while loops.py
@@ -0,0 +1,24 @@
+#rounds = int(input("how many greeting:"))
+#finished_rounds = 0
+#while finished_rounds < rounds:
+#    print("good mornings")
+#    finished_rounds = finished_rounds 
+
+#command = input("Enter command: ")
+#while command != "stop":
+#    print("Executing command: " + command)
+#    command = input("Enter command: ")
+#print("Execution stopped.")
+
+
+#command = input("Enter command: ")
+#while command != "stop":
+#    if command == "MAYDAY":
+#        break
+#    print("Executing command: " + command)
+#    command = input("Enter command: ")
+#print("Execution stopped.")
+
+number = int(input("enter a number"))
+if number == /3 
+print("num is divisible by 3")
---
+++ modules/m5.py
@@ -0,0 +1,56 @@
+#password = input("enter a password")
+#while password != 'word':
+#    print(password)
+##    if password != 'word':
+#       print("sorry, incorrect.! Try again.")
+#    else:
+#        print("entry gained")
+#    print = input("enter a password")
+
+
+#number = 0
+#while number <= 1000:
+#    if number % 3 == 0:
+#        print(number)
+#    number = number + 1
+
+
+
+
+
+#number = 0
+#while number <= 1000:
+#     number = int(input("what is the number "))
+#     if number % 3 == 0:
+#            print(number)
+#            number = number + 1
+
+
+cent = int(input("enter a number"))
+while cent >= 0:
+      print(cent) 
+      print("enter your number")
+      cent = int(input("enter"))
+      if cent < 0:
+            break
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
